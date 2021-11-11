674fa82b12ae49c143f1708dd35355f6e703fca0
Sirasit Boonklang <ssbunklang@gmail.com>
2021-11-12 01:04:30 +0700

    first REST API

--- REST-API.go
+++ REST-API.go
@@ -1,25 +1,17 @@
-//https://docs.microsoft.com/en-us/learn/modules/serverless-go/3-web-api
 package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-)
-/*
-http.HandleFunc("/", handleRoute)
+ )
 
-func handleRequest(w: http:ResponseWriter, r: http.Request) {
-    fmt.Fprintf(w, "My first REST API") 
-}
 
-http.ListenAndServe(":3000", nil)*/
 
-func routeHandler(w http.ResponseWriter, r*http.Request){
-	fmt.Println("route hit")
+ func handleRequest(w http.ResponseWriter, r*http.Request) {
+    fmt.Fprintf(w, "Hello") 
 }
 
 func main(){
-	http.HandleFunc("/", routeHandler)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":3000", nil)
 }
